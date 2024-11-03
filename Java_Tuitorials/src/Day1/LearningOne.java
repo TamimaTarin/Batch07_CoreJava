b1e4f57a833c3abac21e13290ac2b2252fbd3b0c
Tamima Tarin <tarin.aiub@gmail.com>
2024-11-03 11:42:17 +0600

    Day-1, Core Java

---
+++ Java_Tuitorials/src/Day1/LearningOne.java
@@ -0,0 +1,14 @@
+package Day1;
+
+public class LearningOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Welcome To Core Java\n");
+		System.out.println("My Name is Java");
+		
+		
+	}
+
+}
---
+++ Java_Tuitorials/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author TAMIMA
+ *
+ */
+module Java_Tuitorials {
+}
